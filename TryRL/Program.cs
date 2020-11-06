849569ca6bab58ad485fe58fd928c9dc2beed817
Sebastian <sebbebergstrom@hotmail.com>
2020-11-06 15:52:17 +0100

    someting

---
+++ TryRL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Raylib_cs;
+
+namespace TryRL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800,600, "Hello World!");
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+      
+                Raylib.ClearBackground(Color.WHITE);
+      
+                Raylib.DrawCircle(100,100,100,Color.MAGENTA);
+      
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
